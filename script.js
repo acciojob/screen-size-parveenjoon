a61de69bc166a7d8331ec3318218dbdec321f8b2
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-05-31 11:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+
 //your JS code here. If required.
+function updateSizeInfo() {
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  const sizeInfoElement = document.getElementById('windowSize');
+  sizeInfoElement.textContent = `Width: ${width}px, Height: ${height}px`;
+}
+
+
+updateSizeInfo();
+
+
+window.addEventListener('resize', updateSizeInfo);
